176e1260f8cb106f9a43065a0703d4b1f13ad967
josephlevarato <joseph@idusante.fr>
2021-09-13 20:16:53 +0200

    Added new Provider, removed vite manifest package

---
+++ app/Providers/ViteServiceProvider.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\HtmlString;
+use Illuminate\Support\ServiceProvider;
+
+class ViteServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Blade::directive('vite', function(): string
+        {
+            $html = '';
+            $devServerRuns = false;
+            if (app()->environment('local')) {
+                try {
+                    $devServerRuns = file_get_contents(public_path('hot'));
+                } catch (\Exception) {}
+
+                if ($devServerRuns) {
+                    $html = '
+                        <script type="module" src="' . env('ASSET_URL') . '/@vite/client"></script>
+                        <script type="module" src="' . env('ASSET_URL') . '/resources/js/app.js"></script>
+                    ';
+                }
+            }
+            else {
+                $manifest = json_decode(file_get_contents(public_path('dist/manifest.json')), true);
+                $html = '
+                    <script type="module" src="/dist/' . $manifest['resources/js/app.js']['file'] . '"></script>
+                    <link rel="stylesheet" href="/dist/' . $manifest['resources/js/app.js']['css'][0] .'">
+                ';
+            }
+
+            return $html;
+        });
+    }
+}
