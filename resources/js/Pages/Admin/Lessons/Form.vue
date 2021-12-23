e98931060c989eb6970130bb952e6db739e7f433
josephlevarato <joseph@idusante.fr>
2021-12-23 01:35:25 +0100

    Working. Got no time for the calendar tho.

--- resources/js/Pages/Admin/Lessons/Form.vue
+++ resources/js/Pages/Admin/Lessons/Form.vue
@@ -1,18 +1,5 @@
 <template>
     <form @submit.prevent="submit">
-        <div v-for="i in form.occurrence" class="pb-10">
-            <Datepicker
-                v-model="date"
-                locale="fr"
-                cancelText="Annuler"
-                selectText="Confirmer"
-                minutesIncrement="30"
-                inputFormat="dd/MM/yyyy HH:mm"
-            >
-
-            </Datepicker>
-        </div>
-
         <div>
             <jet-label for="title" value="Titre" />
             <jet-input id="title" type="text" class="mt-1 block w-full" v-model="form.title" required autofocus />
@@ -33,48 +20,33 @@
 
         <div class="mt-4 flex">
             <div class="w-3/4">
-                <jet-label for="content" value="Ajouter des occurrences" />
-                <jet-input id="occurrences" type="number" min="0" class="mt-1 block w-full" v-model="form.occurrence" />
-                <jet-input-error :message="form.errors.occurrence" class="mt-2" />
+                <jet-label for="content" value="Occurrences" />
+                <p>Nombre d'occurrences : {{ form.occurrence }}</p>
             </div>
             <div class="w-1/4 flex items-end justify-center">
                 <div>
                     <jet-button type="button" @click="add">Ajouter</jet-button>
+                    <jet-secondary-button @click="remove" class="ml-2">Retirer</jet-secondary-button>
                 </div>
             </div>
+        </div>
 
-
-<!--            <div
-                v-for="(entry, index) in form.schedule"
-                :key="index"
-                class="flex justify-between mt-5"
-            >
-                <div>
-                    <jet-label for="content" value="Sélectionner un jour" />
-                    <select v-model="entry.day">
-                        <option value="" selected="selected">Sélectionner...</option>
-                        <option v-for="option in options" :key="option">{{ option.date }}</option>
-                        <jet-input-error :message="form.errors[`schedule.${index}.day`]" class="mt-2" />
-                    </select>
-                </div>
-                <div>
-                    <jet-label for="content" value="Sélectionner une heure de début (format 00:00)" />
-                    <jet-input type="text" class="mt-1 block w-full" v-model="entry.begin" />
-                    <jet-input-error :message="form.errors[`schedule.${index}.begin`]" class="mt-2" />
-                </div>
-                <div>
-                    <jet-label for="content" value="Sélectionner une heure de fin (format 00:00)" />
-                    <jet-input type="text" class="mt-1 block w-full" v-model="entry.end" />
-                    <jet-input-error :message="form.errors[`schedule.${index}.end`]" class="mt-2" />
-                </div>
-                <div class="flex">
-                    <jet-secondary-button @click="remove(index)">Retirer</jet-secondary-button>
-                </div>
+        <div class="mt-4 flex flex-wrap">
+            <div v-for="(date, i) in dateList" :key="i" class="w-1/4 px-4 mb-2">
+                <jet-label :value="`Date pour l'occurence n°${i + 1}`" />
+                <Datepicker
+                    v-model="date.date"
+                    locale="fr"
+                    cancelText="Annuler"
+                    textInput
+                    format="dd/MM/yyyy HH:mm"
+                    :textInputOptions="textInputOptions"
+                    selectText="Confirmer"
+                    minutesIncrement="15"
+                    inputFormat="dd/MM/yyyy HH:mm"
+                />
+                <jet-input-error :message="form.errors[`schedule.${i}.date`]" class="mt-2" />
             </div>
-            <div class="mt-4 flex justify-center">
-
-            </div>-->
-
         </div>
 
         <div class="mt-8">
@@ -88,16 +60,17 @@
 </template>
 
 <script>
-import JetInput from '@/Jetstream/Input.vue'
-import JetButton from '@/Jetstream/Button.vue'
-import JetLabel from '@/Jetstream/Label.vue'
+import JetInput from '@/Jetstream/Input.vue';
+import JetButton from '@/Jetstream/Button.vue';
+import JetLabel from '@/Jetstream/Label.vue';
 import Wysiwyg from '@/Jetstream/Wysiwyg.vue';
-import JetSecondaryButton from '@/Jetstream/SecondaryButton.vue'
-import JetInputError from '@/Jetstream/InputError.vue'
+import JetSecondaryButton from '@/Jetstream/SecondaryButton.vue';
+import JetInputError from '@/Jetstream/InputError.vue';
 import Datepicker from 'vue3-date-time-picker';
-import 'vue3-date-time-picker/dist/main.css'
+import dayjs from "dayjs";
+import 'vue3-date-time-picker/dist/main.css';
 import {useForm} from "@inertiajs/inertia-vue3";
-import {computed, ref} from "vue";
+import {computed, onMounted, ref} from "vue";
 
 export default {
     title: 'Nouveau cours',
@@ -122,7 +95,13 @@ export default {
     },
 
     setup (props) {
-        const date = ref(new Date());
+        onMounted(() => {
+            if (props.editing) {
+                form.occurrence = props.lesson.schedule.length
+                dateList.value = props.lesson.schedule;
+            }
+        })
+
         const data = props.editing ? {
             ...props.lesson,
             _method: 'PUT',
@@ -130,33 +109,29 @@ export default {
             title: '',
             description: '',
             ref: '',
-            occurrence: 0,
-            schedule: [{ day: '', begin: '', end: ''}],
+            occurrence: 1,
+            schedule: [],
         }
-
         const form = useForm(data);
 
-        const options = [
-            {date: 'Lundi'},
-            {date: 'Mardi'},
-            {date: 'Mercredi'},
-            {date: 'Jeudi'},
-            {date: 'Vendredi'},
-            {date: 'Samedi'},
-            {date: 'Dimanche'},
-        ]
-
-        const schedule = computed(() => Array.isArray(form.schedule) ? form.schedule : JSON.parse(form.schedule))
+        const date = dayjs().startOf('day').toDate()
+        const dateList = ref([{date: date}]);
+        const textInputOptions = ref({
+            format: 'dd.MM.yyyy'
+        })
 
         const add = () => {
-            form.schedule.push({ day: '', begin: '', end: ''})
+            form.occurrence++;
+            dateList.value.push({date: date});
         }
 
-        const remove = (index) => {
-            form.schedule.splice(index, 1)
+        const remove = () => {
+            form.occurrence--;
+            dateList.value.pop();
         }
 
         function submit () {
+            form.schedule = dateList.value;
             const path = props.editing ? route('cours.update', {cour: props.lesson.id}) : route('cours.store');
             form.post(path);
         }
@@ -164,11 +139,10 @@ export default {
         return {
             form,
             submit,
-            schedule,
-            date,
+            dateList,
+            textInputOptions,
             add,
             remove,
-            options,
         }
     },
 }
