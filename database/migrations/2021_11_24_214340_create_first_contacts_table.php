571555cd2a57aa3b66803a927a1d7c5226f9187a
Shift <shift@laravelshift.com>
2022-03-08 23:34:10 +0000

    Adopt anonymous migrations

--- database/migrations/2021_11_24_214340_create_first_contacts_table.php
+++ database/migrations/2021_11_24_214340_create_first_contacts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFirstContactsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -35,4 +35,4 @@ class CreateFirstContactsTable extends Migration
     {
         Schema::dropIfExists('first_contacts');
     }
-}
+};
