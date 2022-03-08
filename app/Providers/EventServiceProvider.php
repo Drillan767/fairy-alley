5d48e28bea2b9dfb047eca42a8db06b7b65a1ada
Shift <shift@laravelshift.com>
2022-03-08 23:34:12 +0000

    Shift cleanup

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -46,4 +46,3 @@ class EventServiceProvider extends ServiceProvider
         return false;
     }
 }
-
--- database/factories/LessonFactory.php
+++ database/factories/LessonFactory.php
@@ -2,7 +2,6 @@
 
 namespace Database\Factories;
 
-use App\Models\Lesson;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class LessonFactory extends Factory
