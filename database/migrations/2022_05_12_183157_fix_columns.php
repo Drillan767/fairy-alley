0cb2a94d475d52d747885167831664452853201e
josephlevarato <joseph@idusante.fr>
2022-05-12 18:33:45 +0200

    Fix column

---
+++ database/migrations/2022_05_12_183157_fix_columns.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('lessons', function (Blueprint $table) {
+            $table->string('color', 7)->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
